5383b5ace063d8e1943d6a728904bfd4c65695c7
jedier stivenson correa amaris <153353269+Stivenco12@users.noreply.github.com>
2024-09-03 17:50:05 -0500

    clase 3/09/2024 segunda parte
    commit porque no me acuerdo si se avanzo mas

---
+++ hecho por mi/8_eliminar/main.js
@@ -0,0 +1,10 @@
+datos = [
+    {id: 1, nombre: "jedier", numero: 3223},
+    {id: 2, nombre: "jedier", numero: 3223},
+    {id: 3, nombre: "jedier", numero: 3223},
+    {id: 4, nombre: "jedier", numero: 3223},
+    {id: 5, nombre: "jedier", numero: 3223},
+]
+
+datos.pop();
+console.log(datos)
